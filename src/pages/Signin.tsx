bfe2f518f6abc043242cd76e54ec6107ef0da356
SungbinYang <ysb05222@naver.com>
2021-06-27 11:46:16 +0900

    로그인, 로그아웃 구현

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import SigninContainer from "../containers/SigninContainer";
+import { RootState } from "../types";
+import { Redirect } from "react-router";
 
 export default function Signin() {
-  return (
-    <div>
-      <h1>Signin</h1>
-    </div>
+  const token = useSelector<RootState, string | null>(
+    (state) => state.auth.token
   );
+
+  if (token !== null) return <Redirect to="/" />;
+
+  return <SigninContainer />;
 }
